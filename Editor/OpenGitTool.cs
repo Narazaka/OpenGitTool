898451cb61a1bea455ac6d5e0e3a378b96b0668c
Narazaka <info@narazaka.net>
2022-07-04 11:51:44 +0900

    init

---
+++ Editor/OpenGitTool.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Diagnostics;
+using System.IO;
+
+public class OpenGitTool {
+    const string GitExtensionsExe = @"C:\Program Files (x86)\GitExtensions\GitExtensions.exe";
+
+    [MenuItem("Open Git Tool/Git Extensions")]
+    static void GitExtensions() {
+        Process.Start(GitExtensionsExe, $"openrepo \"{Directory.GetCurrentDirectory().Replace("\"", "\\\"")}\"");
+    }
+
+    [MenuItem("Open Git Tool/Powershell")]
+    static void Powershell() {
+        Process.Start("powershell");
+    }
+}
